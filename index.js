f8d5453b18a6f4f303de55bae400226bb9ac1d20
ozPop <ozziepopovas@gmail.com>
2016-10-15 18:14:28 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  console.log(katzDeliLine);
+  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  var nextPerson = null;
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+     nextPerson = katzDeliLine.shift();
+     return `Currently serving ${nextPerson}.`;
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var i = 0,
+      output = "The line is currently: ";
+  if ( katzDeliLine.length === 0 ) {
+      return "The line is currently empty.";
+  } else {
+    while ( i < katzDeliLine.length) {
+      output += `${i + 1}. ${katzDeliLine[i]}`;
+      if (i < katzDeliLine.length - 1) {
+        output += ", ";
+      }
+      i++;
+    }
+    return output;
+  }
+}
+
